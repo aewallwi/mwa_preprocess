63c882455a3db652583a31fc6975ed06587f5ae7
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-09-06 14:55:19 -0700

    add clobber arg.

--- scripts/mwa_upload_gdrive.py
+++ scripts/mwa_upload_gdrive.py
@@ -11,9 +11,11 @@ ap.add_argument('--data_folder', type=str, required=True, help="unique identifie
 ap.add_argument('--data_files', type=str, required=True, nargs="+", help="List of datafiles to upload.'")
 ap.add_argument("--sleep_time", type=float, default=0., help="time interval to wait between uploading each file.")
 ap.add_argument("--retry_time", type=float, default=60., help="time interval to wait on a failure to connect before retrying. ")
+ap.add_argument("--clobber", default=False, action="store_true", help="Overwrite already uploaded files on gdrive. ")
+
 #ap.add_argument('--mode', type=str, default='both', help="specify whether to download 'data', 'cal' or 'both'")
 
 args = ap.parse_args()
 
 preprocess.upload_gdrive(data_folder=args.data_folder, data_files=args.data_files,
-                         sleep_time=args.sleep_time, retry_time=args.retry_time)
+                         sleep_time=args.sleep_time, retry_time=args.retry_time, clobber=args.clobber)
